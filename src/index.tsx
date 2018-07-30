df1f534c5a5592c0ad042cdda83f867fc7ba89d0
Samuel Neff <sam@serndesign.com>
2018-07-29 21:01:52 -0400

    package.json scripts updates

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as Case from './Case';
+import * as ForEach from './ForEach';
+import * as If from './If';
+import * as Switch from './Switch';
+
+export {
+    Case,
+    ForEach,
+    If,
+    Switch
+}
